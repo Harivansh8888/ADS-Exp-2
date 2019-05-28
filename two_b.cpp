546551009fcc73fce62027339a1948ba959b6518
Harivansh Mathur <mathurharivansh811@gmail.com>
2019-05-28 23:46:16 +0530

    Added main function

--- two_b.cpp
+++ two_b.cpp
@@ -94,4 +94,31 @@ return (size() == 0);
 bool queue::isFull()
 {
 return (size() == capacity);
-}}
+}
+}
+// Queue implementation in C++
+int main()
+{
+// create a queue of capacity 5
+queue q(5);
+
+q.enqueue(1);
+q.enqueue(2);
+q.enqueue(3);
+cout << "Front element is: " << q.peek() << endl;
+q.dequeue();
+q.enqueue(4);
+
+cout << "Queue size is " << q.size() << endl;
+
+q.dequeue();
+q.dequeue();
+q.dequeue();
+if (q.isEmpty())
+cout << "Queue Is Empty\n";
+else
+cout << "Queue Is Not Empty\n";
+
+return 0;
+}
+
